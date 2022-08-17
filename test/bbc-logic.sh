296ea62ba747e9c0f494a11ef322b93f5aaf8f59
Eva Du Lieu <eve.dulieu@gmail.com>
2022-08-17 22:34:08 +0100

    Added item to output

--- test/bbc-logic.sh
+++ test/bbc-logic.sh
@@ -4,6 +4,8 @@ CURRFULLFILENAME=""
 CURRFileExtension=""
 CURRFILENAME=""
 
+declare -a newFileName
+
 #////////////////////////////////
 function __getFileExt()
 {
@@ -50,7 +52,7 @@ function __renameFile()
         
         local length=${#parts[@]}
         
-        echo $length
+        echo "Len="$length
         
         # only process files that have 3 or more parts
         if [[ $length -gt 2 ]]; then
@@ -64,12 +66,55 @@ function __renameFile()
         
         if [[ "${parts[0]}" == "Top" ]] && [[ "${parts[1]}" == "of" ]] && [[ "${parts[2]}" == "the" ]] && [[ "${parts[3]}" == "Pops" ]]; then
             
-            printf "top of the\n"
+            printf "top of the [$dashFound]\n"
+            
+            if [[ $dashFound -eq 1 ]]; then
+                
+                printf "top of the\n"
+                
+                newFileName+="${parts[0]}."
+                newFileName+="${parts[1]}."
+                newFileName+="${parts[2]}."
+                newFileName+="${parts[3]}."
+                newFileName+="${parts[7]}-"
+                newFileName+="${parts[6]}-"
+                newFileName+="${parts[5]}"
+                
+                
+                #for (( j=0; j<length; j++ ));
+                #do
+                #    printf "${parts[$j]}"
+                #    #newFileName[${#newFileName[@]}]="${parts[$j]}"
+                #    newFileName+="${parts[$j]}"
+                #done
+                
+            fi
+            
+        fi
+        
+        if [[ $dashFound -eq 1 ]] && [[ $dotFound -eq 1 ]]; then
+            
+            printf "dash dot\n"
+            
+            local dashPos=0
+            
+            for (( j=0; j<length; j++ ));
+            do
+                if [[ "${parts[$j]}" == "-" ]]; then
+                    dashPos=j
+                    break
+                else
+                    newFileName+="${parts[j]}."
+                fi
+            done
+            
         fi
         
         ((fileRenamedCnt=fileRenamedCnt+1))
         
     fi
+    
+    printf ">>>>>>$newFileName\n"
     
 }
 
@@ -78,6 +123,6 @@ function __renameFile()
 # Top_of_the_Pops_-_08_04_1993_m0019dvp_original.mp4
 
 
-__renameFile "dir" "Escape_to_the_Country_Series_19_-_14._Welsh_Borders_b0c11gh2_editorial.mp4"
+__renameFile "dir" "Escape_to_the_Country_Series_19_-_14._Welsh_Borders_b0c11gh2_editorial"
 
-__renameFile "dir" "Top_of_the_Pops_-_08_04_1993_m0019dvp_original.mp4"
+#__renameFile "dir" "Top_of_the_Pops_-_08_04_1993_m0019dvp_original"
